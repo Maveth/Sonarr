9d26552e3655cf56ffc7abaab0c30ae03bf6f023
Maveth <memyselfego@hotmail.com>
2018-02-24 12:52:42 -0700

    changed port
    changed refrences of port 8989 to 5656

--- src/NzbDrone.Integration.Test/IntegrationTestBase.cs
+++ src/NzbDrone.Integration.Test/IntegrationTestBase.cs
@@ -161,7 +161,7 @@ namespace NzbDrone.Integration.Test
         protected void ConnectSignalR()
         {
             _signalRReceived = new List<SignalRMessage>();
-            _signalrConnection = new Connection("http://localhost:8989/signalr");
+            _signalrConnection = new Connection("http://localhost:5656/signalr");
             _signalrConnection.Start(new LongPollingTransport()).ContinueWith(task =>
             {
                 if (task.IsFaulted)
